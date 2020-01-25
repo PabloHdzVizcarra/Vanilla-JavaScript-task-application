9809bd2d442ad8555d71e5996324323fd14b1c4b
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-01-24 23:55:13 -0600

    Se deja a la mitad la aplicacion

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(e) {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    
+    const task = {
+        title,
+        description
+    };
+
+    if (localStorage.getItem('tasks') === null) {
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+    e.preventDefault();
+}
+
+function getTasks() {
+    
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let taskView = document.getElementById('tasks')
+    taskView.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(tasks[i]);
+    }
+
+}
+
+getTasks();
