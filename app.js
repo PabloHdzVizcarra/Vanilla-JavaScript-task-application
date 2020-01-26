3a33145c508bb354efd01a2c276bf1a49250b191
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-01-25 23:00:50 -0600

    Se termina la aplicacion

--- app.js
+++ app.js
@@ -19,18 +19,38 @@ function saveTask(e) {
         tasks.push(task);
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }
+    getTasks();
+    document.getElementById('formTask').reset();
     e.preventDefault();
 }
 
 function getTasks() {
     
     let tasks = JSON.parse(localStorage.getItem('tasks'));
-    let taskView = document.getElementById('tasks')
-    taskView.innerHTML = '';
+    let tasksView = document.getElementById('tasks')
+    tasksView.innerHTML = '';
     for (let i = 0; i < tasks.length; i++) {
-        console.log(tasks[i]);
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+
+        tasksView.innerHTML += `<div class="card mb-3">
+            <div class="card-body">
+                <p>${title} - ${description}</p>
+                <a class="btn btn-danger" onclick="deleteTask('${title}')" > Delete</a>
+            </div>
+        </div>`
     }
 
 }
 
+function deleteTask(title) {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    for(let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice([i], 1);
+        }
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTasks();
+}
 getTasks();
